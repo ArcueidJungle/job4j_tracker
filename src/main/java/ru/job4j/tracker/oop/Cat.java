46a92603bba1d84975f944cf6fb5fffd14cfbff2
ArcueidJungle <kaminoonte00@gmail.com>
2022-07-06 15:25:03 +0300

    1.5. Поля объекта [#166083 #277526]

--- src/main/java/ru/job4j/tracker/oop/Cat.java
+++ src/main/java/ru/job4j/tracker/oop/Cat.java
@@ -1,13 +1,31 @@
 package ru.job4j.tracker.oop;
 
 public class Cat {
+    private String food;
+    private String name;
+
     public String voice() {
         return " myau myau";
     }
 
+    public void show() {
+        System.out.println(this.name);
+        System.out.println(this.food);
+    }
+
+    public  void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void giveNick(String nick) {
+        this.name = nick;
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
-        Cat sparky = new Cat();
-        System.out.println("peppy says" + peppy.voice());
+        Cat gav = new Cat();
+        gav.eat("kotleta");
+        gav.giveNick("gav");
+        System.out.println("there are gav food:");
+        gav.show();
     }
 }
