7dae4f9ecc76f997d75283f9939dd433eae40957
ArcueidJungle <kaminoonte00@gmail.com>
2022-07-06 14:37:43 +0300

    2. Вызов метода объекта [#166085 #277132]]

---
+++ src/main/java/ru/job4j/tracker/oop/Student.java
@@ -0,0 +1,18 @@
+package ru.job4j.tracker.oop;
+
+public class Student {
+    public  void music() {
+        System.out.println("Tratatata");
+    }
+
+    public void song() {
+        System.out.println("I believe i can fly");
+    }
+
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.music();
+        petya.music();
+        petya.music();
+    }
+}
