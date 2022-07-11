a24d2518569bad7b8a3c3d19996d8ca04b6b346e
ArcueidJungle <kaminoonte00@gmail.com>
2022-07-11 19:47:03 +0300

    5.1. Тестирование. Подготовка данных. [#182960 #294906]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -24,8 +24,8 @@ public class StartUI {
 
     public static void editItem(Input input, Tracker tracker) {
         System.out.println("=== Edit item ===");
-        int id = input.askInt("Enter id:");
-        String name = input.askStr("Enter name");
+        int id = Integer.parseInt(input.askStr("Enter id: "));
+        String name = input.askStr("Enter name: ");
         Item item = new Item(name);
         if (tracker.replace(id, item)) {
             System.out.println("Заявка изменена успешно.");
@@ -36,7 +36,7 @@ public class StartUI {
 
     public static void deleteItem(Input input, Tracker tracker) {
         System.out.println("=== Delete item ===");
-        int id = input.askInt("Enter id:");
+        int id = Integer.parseInt(input.askStr("Enter id:"));
         if (tracker.delete(id)) {
             System.out.println("Заявка удалена успешно.");
         } else {
