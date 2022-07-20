1f94c0bbbcc637baa23a87c9d1f7ed1470731ac5
ArcueidJungle <kaminoonte00@gmail.com>
2022-07-20 21:05:42 +0300

    1.1. Тесты на StartUI. [#223313]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -148,7 +148,7 @@ public class StartUITest {
     public void whenInvalidExit() {
         Output out = new StubOutput();
         Input in = new StubInput(
-                new String[] {"5" , "0"}
+                new String[] {"5", "0"}
         );
         Tracker tracker = new Tracker();
         UserAction[] actions = new UserAction[]{
