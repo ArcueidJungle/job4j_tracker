fbb27ec86f83ad798b1c8a5f3f776bac17fdc7d5
ArcueidJungle <kaminoonte00@gmail.com>
2023-01-07 22:03:59 +0300

    0. Map, HashMap [#212652]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -6,7 +6,7 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("Leontev@mail.ru", "Леонтьев Иван Иванович");
-        map.put("Pusshkin@gmail.ru", "Пушкин     Александр Сергеевич");
+        map.put("Pushkin@gmail.ru", "Пушкин     Александр Сергеевич");
         map.put("Lavrentev@mail.ru", "Лаврентеьв Иван Иванович");
         for (String key : map.keySet()) {
             String value = map.get(key);
